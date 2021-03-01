8ea2f43aafadccc8181dbe18ecf19469ac1eaa90
Firebasky <63966847+Firebasky@users.noreply.github.com>
2021-03-01 18:29:10 +0800

    Create Struts001.java

---
+++ Struts2/Struts2-001/Struts001.java
@@ -0,0 +1,38 @@
+package com.vulhub.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Struts001 extends ActionSupport {
+    private  String username;
+    private  String password;
+
+
+    @Override
+    public String execute() throws Exception {
+        if ((this.username.isEmpty()) || (this.password.isEmpty())) {
+            return "error";
+        }
+        if ((this.username.equalsIgnoreCase("admin"))
+//                && (this.password.equals("admin"))
+                ) {
+            return "error";
+        }
+        return "error";
+
+    }
+    public void setPassword(String password) {
+        this.password = "%{1+1}";
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
